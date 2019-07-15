2c698a71c14c6f2a02aebd4816c69f18f952bb65
Howard.Zuo <howardzuo@dfocuspace.com>
2019-07-15 19:36:06 +0800

    feat: new version of timezone-db-producer

---
+++ README.md
@@ -0,0 +1,7 @@
+# timezone-db-producer
+
+处理生成时区数据库
+
+## 准备工作
+
+从谷歌日历页面爬取时区数据的原始数据，中英文分别拉取保存在`google_en.json`，`google_zh.json`中。
---
+++ timezone.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+const googleZh = require('./google_zh.json')
+const googleEn = require('./google_en.json')
+
+const langEn = {}
+const canonicals = []
+
+googleEn.forEach(g => {
+  langEn[g[0]] = g[1].replace(/\(.*\)/, '').trim()
+})
+
+googleZh.forEach(g => {
+  canonicals.push({
+    timeZoneName: g[0],
+    timeZoneOffset: g[1]
+      .match(/\(.*\)/)[0]
+      .slice(1, -1)
+      .substring(3),
+    enDisplayName: langEn[g[0]],
+    zhDisplayName: g[1].replace(/\(.*\)/, '').trim(),
+    aliases: g[2]
+  })
+})
+
+fs.writeFileSync('./timezone_v2.json', JSON.stringify(canonicals, null, 2), {
+  encoding: 'utf8'
+})
